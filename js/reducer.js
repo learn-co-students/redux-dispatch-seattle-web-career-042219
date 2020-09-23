0f2326779aec59f0c6a8541a8135834fea976b62
Milan Dhar <milan.dhar@gmail.com>
2020-09-23 21:46:48 +0000

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -7,7 +7,21 @@ function changeState(state, action){
   }
 }
 
-let state = {count: 0}
+function dispatch(action){
+  state = changeState(state, action)
+  return state
+  render()
+}
+
+var state = {count: 0}
 let action = {type: 'INCREASE_COUNT'}
 
 changeState(state, action)
+
+function render(){
+  document.body.textContent = state.count
+}
+
+dispatch({type: 'INCREASE_COUNT'})
+dispatch({type: 'INCREASE_COUNT'})
+render()
